23ebd940ab6541a7c191ebeffebf7efe4a18409d
Shunta Iketaki <s.iketaki@gmail.com>
2021-09-18 01:31:47 +0900

    Add 'loadedJS' config property

--- MMM-WebView.js
+++ MMM-WebView.js
@@ -6,15 +6,30 @@
  */
 
 Module.register('MMM-WebView', {
-	defaults: {
-		url: 'https://www.google.com/',
-		height: '640px',
-		width: '480px',
-	},
-	getDom: function () {
-		let wrapper = document.createElement('div');
-		wrapper.id = 'mmm-webview-wrapper';
-		wrapper.innerHTML = `<webview style="width: ${this.config.width}; height: ${this.config.height};" src="${this.config.url}"></webview>`;
-		return wrapper;
-	},
+  defaults: {
+    url: 'https://www.google.com/',
+    height: '640px',
+    width: '480px',
+    loadedJS: undefined,
+  },
+  getDom: function () {
+    let wrapper = document.createElement('div');
+    wrapper.id = 'mmm-webview-wrapper';
+    wrapper.innerHTML = `<webview id="mmm-webview" style="width: ${this.config.width}; height: ${this.config.height};" src="${this.config.url}"></webview>`;
+    return wrapper;
+  },
+  notificationReceived: function (notification, payload, sender) {
+    if (notification == 'MODULE_DOM_CREATED') {
+      if (this.config.loadedJS && this.config.loadedJS.length > 0) {
+        const webview = document.getElementById('mmm-webview');
+        if (webview) {
+          webview.addEventListener('did-finish-load', () => {
+            webview.executeJavaScript(this.config.loadedJS);
+          });
+        } else {
+          // TODO: Show Error
+        }
+      }
+    }
+  },
 });
