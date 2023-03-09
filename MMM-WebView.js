b302185336c4b0340a99a03d10ff6ec054399941
late4marshmellow <79985161+late4marshmellow@users.noreply.github.com>
2023-03-09 01:16:06 +0100

    Update MMM-WebView.js

--- MMM-WebView.js
+++ MMM-WebView.js
@@ -16,9 +16,10 @@ Module.register('MMM-WebView', {
     autoRefreshInterval: 10 * 60 * 1000, // Refresh interval in milliseconds (10 minutes)
     refreshMinute: 5, // Set the minute at which to refresh
     useRefreshMinute: true, // Set to false to use autoRefreshInterval instead of refreshMinute
-    invertColors: false, // new config option for inverting colors
+    invertColors: false,
     filterPercent: 100, // new config option for filter percentage
     loadedJS: undefined,
+    showScrollbar: true,
   },
 
   start: function() {
@@ -41,14 +42,15 @@ Module.register('MMM-WebView', {
     }
   },
 
-getDom: function () {
-  let wrapper = document.createElement('div');
-  wrapper.id = 'mmm-webview-wrapper';
-  let filter = this.config.invertColors ? `invert(${this.config.filterPercent}%)` : 'none';
-  wrapper.innerHTML = `<webview id="${WEBVIEW_ID}" style="width: ${this.config.width}; height: ${this.config.height}; filter: ${filter};" src="${this.config.url}"></webview>`;
-  return wrapper;
-},
-
+  getDom: function () {
+    let wrapper = document.createElement('div');
+    wrapper.id = 'mmm-webview-wrapper';
+	wrapper.classList.add('mmm-webview'); 
+    wrapper.style.overflow = this.config.showScrollbar ? 'scroll' : 'hidden';
+    let filter = this.config.invertColors ? `invert(${this.config.filterPercent}%)` : 'none';
+    wrapper.innerHTML = `<webview id="${WEBVIEW_ID}" style="width: ${this.config.width}; height: ${this.config.height}; filter: ${filter};" src="${this.config.url}"></webview>`;
+    return wrapper;
+  },
 
   notificationReceived: function(notification, payload, sender) {
     if (notification == 'MODULE_DOM_CREATED') {
@@ -64,4 +66,42 @@ getDom: function () {
       }
     }
   },
+
+  getStyles: function () {
+    return ['MMM-WebView.css'];
+  },
+
+  getScripts: function() {
+    return ['moment.js'];
+  },
+
+  toggleScrollbar: function() {
+    this.config.showScrollbar = !this.config.showScrollbar;
+    const wrapper = document.getElementById('mmm-webview-wrapper');
+    if (this.config.showScrollbar) {
+      wrapper.style.overflow = 'scroll';
+    } else {
+      wrapper.style.overflow = 'hidden';
+    }
+  },
+
+  notificationReceived: function(notification, payload, sender) {
+    if (notification === 'MMM_WEBVIEW_TOGGLE_SCROLLBAR') {
+      this.toggleScrollbar();
+    }
+  },
+
+  suspend: function() {
+    const webview = document.getElementById(WEBVIEW_ID);
+    if (webview) {
+      webview.src = '';
+    }
+  },
+
+  resume: function() {
+    const webview = document.getElementById(WEBVIEW_ID);
+    if (webview) {
+      webview.src = this.config.url;
+    }
+  }
 });
